bc1196df4555683ed995f97a16936351d1a48937
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-24 23:01:24 -0500

    Update index.py

--- index.py
+++ index.py
@@ -18,3 +18,5 @@ for i in range(num_experimentos):
     datos_experimentos.append(datos_experimento)
     # Visualizar los datos
 plt.figure(figsize=(10, 6))
+for i in range(num_experimentos):
+    plt.plot(tiempo, datos_experimentos[i], label=f'Experimento {i + 1}')

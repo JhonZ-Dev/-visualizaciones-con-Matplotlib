43d355dc256893c3b9ebcb4d5fdf1228e0906ae2
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-25 23:03:59 -0500

    Update index.py

--- index.py
+++ index.py
@@ -12,7 +12,13 @@ num_puntos = 100
 tiempo = np.linspace(0, 1, num_puntos)
 # Crear datos de experimentos con diferentes tendencias y ruido
 datos_experimentos = []
-
+for i in range(num_experimentos):
+    tendencia = 0.5 * i * tiempo  # Tendencia lineal creciente
+    ruido = np.random.normal(0, 0.1, num_puntos)  # Ruido gaussiano
+    datos_experimento = tendencia + ruido
+    datos_experimentos.append(datos_experimento)
+    # Visualizar los datos
+plt.figure(figsize=(10, 6))
 for i in range(num_experimentos):
     plt.plot(tiempo, datos_experimentos[i], label=f'Experimento {i + 1}')
 

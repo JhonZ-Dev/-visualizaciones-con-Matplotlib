72ad9dee953e67db33531601eb5ea2a0be0630c0
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-25 23:03:39 -0500

    Update index.py

--- index.py
+++ index.py
@@ -3,15 +3,7 @@ import matplotlib.pyplot as plt
 
 
 # Generar datos aleatorios para un conjunto de experimentos
-np.random.seed(42)
-# Número de experimentos
-num_experimentos = 5
-# Número de puntos de datos por experimento
-num_puntos = 100
-# Crear datos de tiempo (en segundos)
-tiempo = np.linspace(0, 1, num_puntos)
-# Crear datos de experimentos con diferentes tendencias y ruido
-datos_experimentos = []
+
 for i in range(num_experimentos):
     tendencia = 0.5 * i * tiempo  # Tendencia lineal creciente
     ruido = np.random.normal(0, 0.1, num_puntos)  # Ruido gaussiano
